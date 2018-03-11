7dbff7de35c24fd997110998b2df16d163856d15
Dustin <dfite85@gmail.com>
2018-03-11 23:43:21 +0000

    mentor avail?

--- lib/kele.rb
+++ lib/kele.rb
@@ -18,6 +18,11 @@ class Kele
       end
     end
   end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
 
   private
 
